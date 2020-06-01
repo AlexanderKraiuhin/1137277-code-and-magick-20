b8b7991ae2362597d03b86b66714caf6252caa8c
Alexander <64850277+AlexanderKraiuhin@users.noreply.github.com>
2020-06-01 16:33:31 +0300

    Add files via upload

--- stat.js
+++ stat.js
@@ -12,7 +12,7 @@ var BAR_WIDTH = 40;
 var BAR_GAP = 50;
 var BAR_HEIGHT = 150;
 
-var renderCloud = function(ctx, x, y, color, textArr) {
+var renderCloud = function (ctx, x, y, color, textArr) {
     ctx.fillStyle = color;
     ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 
@@ -27,7 +27,7 @@ var renderCloud = function(ctx, x, y, color, textArr) {
     }
   };
 
-var getMaxElement = function(arr) {
+var getMaxElement = function (arr) {
     var maxElement = arr[0];
     
     for (var i = 0; i < arr.length; i++) {
@@ -39,14 +39,14 @@ var getMaxElement = function(arr) {
     return maxElement;
 };
 
-function randomColor() {
+function randomColor () {
     var h = 202;
     var s = Math.floor(Math.random() * 101);
     var l = 50;
     return 'hsl('+ h +', '+ s +'%,'+ l +'%)'; 
 }
 
-window.renderStatistics = function(ctx, players, times) {
+window.renderStatistics = function (ctx, players, times) {
     renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
     renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff', TITLE_ARR);
     
@@ -64,4 +64,4 @@ window.renderStatistics = function(ctx, players, times) {
         ctx.fillStyle = 'black';
         ctx.fillText(Math.floor(times[i]), CLOUD_X + GAP * 2 + FONT_GAP + (BAR_GAP + BAR_WIDTH) * i, CLOUD_Y + (GAP + FONT_GAP) * 2 + BAR_HEIGHT - (BAR_HEIGHT * times[i]) / maxTime);
     }
-}
+};
